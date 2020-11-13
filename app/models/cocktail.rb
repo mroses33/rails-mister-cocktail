935901d5143af42b124ea0eed3f5f1ec9a648650
Mollie Semmes <mollie.rose279@gmail.com>
2020-11-13 14:57:36 +0000

    updated show page

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_many :reviews, dependent: :destroy
+  has_many :ratings, through: :reviews
+  has_one_attached :photo
   validates :name, presence: true, uniqueness: true
 end
