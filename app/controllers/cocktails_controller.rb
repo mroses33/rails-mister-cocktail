697e48d38ec99f2c5df764b0278029ddf69ade0d
Mollie Semmes <mollie.rose279@gmail.com>
2020-11-13 16:51:33 +0000

    added delete route

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,6 +21,12 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+  redirect_to cocktails_path
+  end
+
   def score
     # @cocktail.ratings
   end
